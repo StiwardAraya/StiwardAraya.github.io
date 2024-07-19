a124e466d88f855ad6665958cab5de3c245570df
Stewart Araya Calderón <StiwardArayaCalderon98@gmail.com>
2024-07-19 16:29:19 -0600

    Change(front): Add documentation to the js document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,37 @@
+/*
+* toggleMenu
+*
+* Defines a boolean variable that indicates if the menu is visible or not
+* and uses it as a condition in order to assign a css class to the menu.
+* 
+* This function is called everytime the responsive menu is clicked to make 
+* the menu visible or to hide it.
+*/
 let visibleMenu = false;
 function toggleMenu(){
     document.getElementById("nav").classList = visibleMenu ? "" : "responsive";
     visibleMenu = !visibleMenu;
 }
 
+/*
+* select
+*
+* Hides the responsive menu when an option is selected
+*/
 function select(){
     document.getElementById("nav").classList = "";
     visibleMenu = false;
 }
 
+
+/*
+* skillsFX
+* Detects when the window scroll is at 300 pixels distance relative to 
+* the top of the skills section.
+*
+* When the window scroll is near the section the function add the css animation
+* classes to every each skill bar.
+*/
 function skillsFX(){
     const skills = document.getElementById("skills");
     const skillsDistance = window.innerHeight - skills.getBoundingClientRect().top;
@@ -27,6 +50,7 @@ function skillsFX(){
     }
 }
 
+//Calls skillsFX when the user scrolls
 window.onscroll = function(){
     skillsFX();
 }
