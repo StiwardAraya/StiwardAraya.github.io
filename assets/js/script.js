c1d187124953fc70fae47ac0b3c5bed8561497ae
StiwardAraya <StiwardArayaCalderon98@gmail.com>
2024-08-08 07:48:03 -0600

    Change(front): Implement 'toggle' function for the responsive menu & update the code to modern JS conventions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,29 @@
-/*
+/**
 * toggleMenu
 *
-* Defines a boolean variable that indicates if the menu is visible or not
-* and uses it as a condition in order to assign a css class to the menu.
-* 
-* This function is called everytime the responsive menu is clicked to make 
-* the menu visible or to hide it.
+* Uses a callback function in the responsive button
+* element's click event.
+*
+* It changes the nav element's class by the 'toggle' 
+* function of classList
 */
-let visibleMenu = false;
-function toggleMenu(){
-    document.getElementById("nav").classList = visibleMenu ? "" : "responsive";
-    visibleMenu = !visibleMenu;
-}
+const responsiveBtn = document.getElementById("responsive-btn");
+responsiveBtn.addEventListener("click", ()=> {
+  document.getElementById("nav").classList.toggle("responsive");
+});
 
-/*
+/**
 * select
 *
 * Hides the responsive menu when an option is selected
 */
-function select(){
-    document.getElementById("nav").classList = "";
-    visibleMenu = false;
+const select = () => {
+  document.getElementById("nav").classList = "";
+  visibleMenu = false;
 }
 
 
-/*
+/**
 * skillsFX
 *
 * Detects when the window scroll is at 300 pixels distance relative to 
@@ -33,22 +32,22 @@ function select(){
 * When the window scroll is near the section the function add the css animation
 * classes to every each skill bar.
 */
-function skillsFX(){
-    const skills = document.getElementById("skills");
-    const skillsDistance = window.innerHeight - skills.getBoundingClientRect().top;
-    if(skillsDistance >= 300){
-        const skillsBars = document.getElementsByClassName("progress");
-        skillsBars[0].classList.add("html");
-        skillsBars[1].classList.add("css");
-        skillsBars[2].classList.add("javascript");
-        skillsBars[3].classList.add("java");
-        skillsBars[4].classList.add("sql");
-        skillsBars[5].classList.add("react");
-        skillsBars[6].classList.add("communication");
-        skillsBars[7].classList.add("problem-solving");
-        skillsBars[8].classList.add("adaptability");
-        skillsBars[9].classList.add("creativity");
-    }
+const skillsFX = () => {
+  const skills = document.getElementById("skills");
+  const skillsDistance = window.innerHeight - skills.getBoundingClientRect().top;
+  if(skillsDistance >= 300){
+    const skillsBars = document.getElementsByClassName("progress");
+    skillsBars[0].classList.add("html");
+    skillsBars[1].classList.add("css");
+    skillsBars[2].classList.add("javascript");
+    skillsBars[3].classList.add("java");
+    skillsBars[4].classList.add("sql");
+    skillsBars[5].classList.add("react");
+    skillsBars[6].classList.add("communication");
+    skillsBars[7].classList.add("problem-solving");
+    skillsBars[8].classList.add("adaptability");
+    skillsBars[9].classList.add("creativity");
+  }
 }
 
 //Calls skillsFX when the user scrolls
@@ -56,14 +55,14 @@ window.onscroll = function(){
     skillsFX();
 }
 
-/*
+/**
 * downloadCV
 * 
 * Creates a link element with a href in the CV local URL,
 * add the link to the button as a child, triggers the link and then
 * removes the link from the button.
 */
-function downloadCV(){
+const downloadCV = () => {
     const link = document.createElement('a');
     link.href = 'assets/documents/Stiward CV.pdf';
     link.target = '_blank';
